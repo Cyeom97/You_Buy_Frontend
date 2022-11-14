a3ed966b11212bb37c4a09964fd96c1ecb7979db
Christopher Yeom <chrisyeom97@gmail.com>
2022-11-14 15:14:13 -0500

    made changes to profie.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,15 +3,17 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const Profile = ({ user, authenticated }) => {
-  const [products, setProducts] = useState([])
   let { id } = useParams()
+  const [products, setProducts] = useState([])
+  const [form, setForm] = useState({
+    name: '',
+    description: '',
+    image: '',
+    price: '',
+    ownerId: parseInt(id)
+  })
+
   let navigate = useNavigate()
-
-  const viewProduct = (oneProduct) => {
-    navigate(`${oneProduct}`)
-  }
-
-  console.log(user.id)
 
   useEffect(() => {
     const handleUser = async () => {
@@ -23,6 +25,20 @@ const Profile = ({ user, authenticated }) => {
     handleUser()
   }, [id])
 
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let newProduct = await axios.post(
+      `http://localhost:3001/profile/${id}`,
+      form
+    )
+    setProducts([...products, newProduct.data])
+    setForm({ name: '', description: '', image: '', price: '' })
+  }
+
   return user && authenticated ? (
     <div>
       <section className="container-grid">
@@ -30,9 +46,26 @@ const Profile = ({ user, authenticated }) => {
           <div>
             <h2>{product.name}</h2>
             <img src={product.image} alt={product.name} className="img" />
+            <h3>{product.description}</h3>
+            <h3>{product.price}</h3>
           </div>
         ))}
       </section>
+      <form onSubmit={handleSubmit} className="form-type">
+        <label htmlFor="name">Name:</label>
+        <input id="name" value={form.name} onChange={handleChange}></input>
+        <label htmlFor="image">Image URL:</label>
+        <input id="image" value={form.image} onChange={handleChange}></input>
+        <label htmlFor="description">Description:</label>
+        <input
+          id="description"
+          value={form.description}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor="price">Price:</label>
+        <input id="price" value={form.price} onChange={handleChange}></input>
+        <button type="submit">Add Product</button>
+      </form>
     </div>
   ) : (
     <div>
