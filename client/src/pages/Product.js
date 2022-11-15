9995c5081c1029dc2dbdda0a143e1f2663349bcc
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-11-15 14:14:45 -0600

    preparing for pull

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ViewProduct from '../components/ViewProduct'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const GetProduct = () => {
@@ -14,9 +13,10 @@ const GetProduct = () => {
     const apiCall = async () => {
       const response = await axios.get(`http://localhost:3001/products/${id}`)
       setSelectedProduct(response.data)
+      console.log(response.data)
     }
     apiCall()
-  })
+  }, [])
   return (
     <div>
       <div>{selectedProduct && <ViewProduct product={selectedProduct} />}</div>
